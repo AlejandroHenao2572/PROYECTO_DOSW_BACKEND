b3db2da3e88f69983c1dd6dbb64777da7e24ed09
Jared <Jared.farfan-g@mail.escuelaing.edu.co>
2025-09-18 11:22:23 -0500

    conexion DB jared

--- src/main/java/com/sirha/proyecto_sirha_dosw/repository/UsuarioRepository.java
+++ src/main/java/com/sirha/proyecto_sirha_dosw/repository/UsuarioRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import com.sirha.proyecto_sirha_dosw.model.Usuario;
 
+
 import java.util.Optional;
 
 @Repository
